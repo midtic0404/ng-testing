a003a868aae12cde6c281e58422b2c58176d8d53
Chia Che Chang <midtic0404@gmail.com>
2020-02-14 23:57:55 -0700

    update

--- src/app/courses/courses-card-list/courses-card-list.component.spec.ts
+++ src/app/courses/courses-card-list/courses-card-list.component.spec.ts
@@ -15,6 +15,7 @@ describe('CoursesCardListComponent', () => {
 
   let component: CoursesCardListComponent;
   let fixture: ComponentFixture<CoursesCardListComponent>;
+  let el: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -23,6 +24,7 @@ describe('CoursesCardListComponent', () => {
     .then(() => {
       fixture = TestBed.createComponent(CoursesCardListComponent);
       component = fixture.componentInstance;
+      el = fixture.debugElement;
     })
   }));
 
@@ -37,7 +39,11 @@ describe('CoursesCardListComponent', () => {
 
   it("should display the course list", () => {
 
-    pending();
+    component.courses = setupCourses();
+    fixture.detectChanges();
+    const cards = el.queryAll(By.css(".course-card"));
+    expect(cards).toBeTruthy('Could not find cards');
+    expect(cards.length).toBe(12, 'Unexpected number of cards');
 
   });
 
